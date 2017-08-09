f424bf105ee84fe4e667e1c5655f590769705cce
vmarcosp <marcosvcontato.mv@gmail.com>
2017-08-09 00:26:27 -0300

    Added gulp task for build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    cleanCSS = require('gulp-clean-css');
 
 gulp.task('serve', ['sass'], () => {
 
@@ -9,8 +10,19 @@ gulp.task('serve', ['sass'], () => {
     });
     gulp.watch('scss/*.scss', ['sass']);
 
-    gulp.watch('index.html').on('change', browserSync.reload); 
+    gulp.watch('index.html').on('change', browserSync.reload);
 
+});
+
+gulp.task('move-files', ['sass'], () => {
+    gulp.src(['./index.html', 'img/**/*'], { base: '.' })
+        .pipe(gulp.dest('docs'));
+});
+
+gulp.task('minify-css', () => {
+    return gulp.src('css/*.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('docs/css'));
 });
 
 gulp.task('sass', function () {
@@ -21,3 +33,4 @@ gulp.task('sass', function () {
 });
 
 gulp.task('default', ['serve']);
+gulp.task('build', ['move-files','minify-css']);
